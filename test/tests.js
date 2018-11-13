ae03880729446149e5ed4f4189b1dc0a55e560e5
Rob Hicks <rob@hixfamily.org>
2018-11-13 16:51:59 -0700

    ignore dist files and start adding tests

---
+++ test/tests.js
@@ -0,0 +1,6 @@
+global.assert = require('chai').assert;
+global.chai = require('chai');
+global.expect = require('chai').expect;
+global.sinon = require('sinon');
+global.fetchMock = require('fetch-mock');
+global.fetch = require('node-fetch');
